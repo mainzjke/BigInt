32da550e554f3af76cae2f7f1b4a9ccb9b9677bb
mainzjke <andrey.kurnikov1998@mail.ru>
2024-12-20 02:52:07 +0400

    added a function for bitwise output of a number

--- src/BigInt/bigint.h
+++ src/BigInt/bigint.h
@@ -4,4 +4,36 @@ class BigInt
 {
 public:
     BigInt();
+    BigInt(const BigInt& other);
+    BigInt(BigInt&& other);
+    BigInt(int val);
+    BigInt(const char *val);
+    ~BigInt();
+
+    BigInt operator +(const BigInt& other);
+    BigInt& operator +=(const BigInt& other);
+
+    BigInt operator -(const BigInt& other);
+    BigInt operator -=(const BigInt& other);
+
+    BigInt operator *(const BigInt& other);
+    BigInt operator *=(const BigInt& other);
+
+    BigInt operator /(const BigInt& other);
+    BigInt operator /=(const BigInt& other);
+
+    BigInt& operator =(const BigInt& other);
+
+
+    bool operator ==(const BigInt& other) const;
+    bool operator !=(const BigInt& other) const;
+    bool operator <(const BigInt& other) const;
+    bool operator <=(const BigInt& other) const;
+
+    bool operator >(const BigInt& other) const;
+    bool operator >=(const BigInt& other) const;
+
+private:
+    char *value;
+    unsigned int size;
 };
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -3,7 +3,5 @@
 #include <bigint.h>
 
 int main(){
-    std::cout << __FUNCTION__ << std::endl;
-    BigInt big;
-  
+    BigInt big(280);
 }
